b255351f50d28bd006494ba777d095884f1804c3
Bowen <BowenLiu0001@gmail.com>
2024-06-20 02:18:24 +0800

    Update code.

--- axi_stream_insert_header.v
+++ axi_stream_insert_header.v
@@ -24,36 +24,32 @@ module axi_stream_insert_header #(
     input       [BYTE_CNT_WD-1 : 0]         byte_insert_cnt,
     output                                  ready_insert    
 );
+    // header data
     reg        [DATA_WD-1 : 0]              header_data_r;
     reg                                     header_valid_r;
     reg        [DATA_BYTE_WD-1 : 0]         header_keep_r;
     reg        [BYTE_CNT_WD-1 : 0]          header_byte_cnt_r;
-
+    reg        [BYTE_CNT_WD-1 : 0]          header_byte_cnt_r2;
+    // data
+    reg                                     data_valid_r;
     reg                                     data_last_r;
-
-    //extended for align
+    // extended for concat
     reg        [DATA_WD*2-1:0]              data_extended;         
     wire       [DATA_WD*2-1:0]              data_aligned;                          
-
     reg        [DATA_BYTE_WD*2-1:0]         keep_extended;
-    reg        [DATA_BYTE_WD*2-1:0]         keep_aligned;
-
-    reg                                     data_valid_r;
-
-    wire                                    header_ready;
-    reg                                     first_data_r;
+    wire        [DATA_BYTE_WD*2-1:0]         keep_aligned;
+    // handshake
+    wire                                    header_handshake;
+    wire                                    data_handshake;
     reg                                     data_handshake_r;
 
-    assign      ready_insert = !header_valid_r || valid_in && last_in;//last data
-
-    wire        header_handshake;
+    wire                                    not_finish;
+    wire                                    header_pulse_r;
+    // --------------------------------------------------------
+    //header ready
+    assign      ready_insert     = !header_valid_r || valid_in && last_in;//not valid || last in
     assign      header_handshake = valid_insert && ready_insert;
-
-    assign      ready_in = header_valid_r  && (!data_valid_r || ready_out);// TODO
-    wire        data_handshake;
-    assign      data_handshake = valid_in && ready_in;
-
-
+    //  header valid
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
             header_valid_r <= 0;
@@ -62,38 +58,44 @@ module axi_stream_insert_header #(
                 header_valid_r <= valid_insert;
         end
     end
-
+    // header data
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             header_byte_cnt_r <= 0;
+            header_byte_cnt_r2 <= 0;
             header_data_r <= 0;
             header_keep_r <= 0;
         end
         else begin
             if (header_handshake) begin
                 header_byte_cnt_r <= byte_insert_cnt;
+                header_byte_cnt_r2 <= header_byte_cnt_r;
                 header_data_r <= data_insert;
                 header_keep_r <= keep_insert;
             end
         end
     end
 
+    // data ready
+    assign      ready_in = header_valid_r  && (!data_valid_r || ready_out);// TODO
+    assign      data_handshake = valid_in && ready_in;
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) data_handshake_r <= 0;
         else data_handshake_r <= data_handshake;
     end
-
+    // data valid
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) data_valid_r <= 0;
         else begin
-            if (ready_out)
-                data_valid_r <= valid_in;
+            if (ready_in)
+                data_valid_r <= valid_in && header_valid_r;
         end
     end
-
+    // data
     always @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
+        if (!rst_n) begin
             data_last_r <= 0;
+        end
         else begin
             if (data_handshake)
                 data_last_r <= last_in;
@@ -101,63 +103,47 @@ module axi_stream_insert_header #(
                 data_last_r <= 0;
         end
     end
-
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
-            first_data_r <= 0;
-        else begin
-            if (!keep_extended[DATA_BYTE_WD*2-1:DATA_BYTE_WD] && keep_extended[DATA_BYTE_WD-1:0])
-                first_data_r <= 1;
-            else
-                first_data_r <= 0;
-        end
-    end
-    reg header_pulse_r;
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) header_pulse_r <= 0;
-        else begin
-            if (valid_insert && !header_pulse_r)
-                header_pulse_r <= 1;
-            else
-                header_pulse_r <= 0;
-        end
-    end
-    
+    assign not_finish       = |keep_aligned[DATA_BYTE_WD-1:0];
+    assign header_pulse_r   = data_handshake & !data_handshake_r & !not_finish;
+    // concat header and data
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             data_extended <= 0;
             keep_extended <= 0;
         end
         else begin
-            // if (header_handshake) begin
-            //     data_extended[DATA_WD-1:0] <= data_insert;
-            //     keep_extended[DATA_BYTE_WD-1:0] <= keep_insert;
-            // end
-            if (data_handshake) begin
+            if (data_handshake || (data_last_r && (|keep_aligned[DATA_BYTE_WD*2-1:0]))) begin
                 if (header_pulse_r) begin
-                data_extended[DATA_WD*2-1:0] <= {header_data_r, data_in};
-                keep_extended[DATA_BYTE_WD*2-1:0] <= {header_keep_r, keep_in};
+                    data_extended[DATA_WD*2-1:0] <= {header_data_r, data_in};
+                    keep_extended[DATA_BYTE_WD*2-1:0] <= {header_keep_r, keep_in};
                 end else begin
-                data_extended[DATA_WD*2-1:0] <= {data_extended[DATA_WD-1:0], data_in};
-                keep_extended[DATA_BYTE_WD*2-1:0] <= {keep_extended[DATA_BYTE_WD-1:0], keep_in};
+                    data_extended[DATA_WD*2-1:0] <= {data_extended[DATA_WD-1:0], header_valid_r ? data_in : {DATA_WD{1'b0}}};
+                    keep_extended[DATA_BYTE_WD*2-1:0] <= {keep_extended[DATA_BYTE_WD-1:0], header_valid_r ? keep_in : {DATA_BYTE_WD{1'b0}}};
                 end
             end
-
         end
     end
-
-    wire [BYTE_CNT_WD-1:0] empty_byte_cnt;
-    wire [BYTE_CNT_WD+3-1:0] empty_byte_cnt_bit;
-
-    assign empty_byte_cnt = DATA_BYTE_WD - header_byte_cnt_r; // empty bits before header
-    assign empty_byte_cnt_bit = empty_byte_cnt << 3;
-
+    // empty shift num
+    reg     [BYTE_CNT_WD-1:0]      empty_byte_cnt_r;
+    wire    [BYTE_CNT_WD+3-1:0]    empty_byte_cnt_bit;
+    wire    [BYTE_CNT_WD-1:0]      header_byte_cnt = (not_finish & valid_insert) ? header_byte_cnt_r2 : header_byte_cnt_r; //r2 avoid overwriting
+    wire    [BYTE_CNT_WD-1:0] temp = DATA_BYTE_WD - header_byte_cnt;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) empty_byte_cnt_r <= 0;
+        else begin
+            if (header_pulse_r) begin
+                empty_byte_cnt_r <= temp;
+            end
+        end
+    end
+    assign empty_byte_cnt_bit = empty_byte_cnt_r << 3;
+    // data and keep by shift
     assign data_aligned = data_extended << empty_byte_cnt_bit;
-    assign data_out = data_aligned[DATA_WD*2-1:DATA_WD]; 
-    assign keep_aligned = keep_extended[DATA_BYTE_WD*2-1:0] << empty_byte_cnt;
-    assign keep_out = data_last_r ? keep_aligned[DATA_BYTE_WD-1:0]: keep_aligned[DATA_BYTE_WD*2-1:DATA_BYTE_WD]; 
+    assign data_out     = data_aligned[DATA_WD*2-1:DATA_WD]; 
+    assign keep_aligned = keep_extended[DATA_BYTE_WD*2-1:0] << empty_byte_cnt_r;
+    assign keep_out     = keep_aligned[DATA_BYTE_WD*2-1:DATA_BYTE_WD]; 
 
-    assign valid_out = data_handshake_r;// 再或上最后多的keep不为0
-    assign last_out = data_valid_r ? data_last_r : 0;
+    assign valid_out    = data_handshake_r || last_out;
+    assign last_out     = |keep_aligned[DATA_BYTE_WD*2-1:DATA_BYTE_WD] && !not_finish;
 
 endmodule
--- tb_axi_stream_insert_header_random.sv
+++ tb_axi_stream_insert_header_random.sv
@@ -61,7 +61,7 @@ axi_stream_insert_header #(
     .valid_in                ( valid_in                              ),
     .data_in                 ( data_in          [DATA_WD-1 : 0]      ),
     .keep_in                 ( keep_in          [DATA_BYTE_WD-1 : 0] ),
-    .last_in                 ( last_in_r                               ),
+    .last_in                 ( last_in                               ),
     .ready_out               ( ready_out                             ),
     .valid_insert            ( valid_insert                          ),
     .data_insert             ( data_insert      [DATA_WD-1 : 0]      ),
@@ -76,27 +76,30 @@ axi_stream_insert_header #(
     .ready_insert            ( ready_insert                          )
 );
 
-reg last_in_r = 0;
+reg data_sig;
+reg last_sig;
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         last_in <= 0;
         keep_in <= 0;
-        last_in_r <= 0;
+        
     end
     else begin
-        if (valid_in) begin
-            last_in_r <= last_in;
-            if (last_in_r) begin
-                last_in <= 0;
-                last_in_r <= 0;
+        if (data_sig) begin
+            
+            if (last_sig & !last_in) begin
+                // keep_in <= 4'b1100;
+                last_cnt = $urandom_range(0, DATA_BYTE_WD-1);
+		        keep_in = 4'hf << last_cnt;
+                last_in <= 1;
+            end else 
                 keep_in <= 4'b1111;
-            end
-            else if (last_in) begin
-                keep_in <= 4'b1100;
-            end
-            else keep_in <= 4'b1111;
         end
+        if (last_sig & !last_in) begin
+            
+        end
+        else last_in <= 0;
     end
 end
 
@@ -105,48 +108,113 @@ always @(posedge clk or negedge rst_n) begin
         data_in <= 32'h01020304;
     end
     else begin
-        if (valid_in & ready_in) begin
+        valid_in <= data_sig;
+        if (data_sig & ready_in) begin
             data_in <= data_in + 32'h04040404;
         end
     end
 end
 
+reg head_sig;
 
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        data_insert <= 32'hAABBCC00;
+        keep_insert <= 0;
+        byte_insert_cnt <= 0;
+    end
+    else begin
+        valid_insert <= head_sig;
+        // byte_insert_cnt <= 0;
+        if (head_sig & ready_insert) begin
+            data_insert <= data_insert + 32'h00000011;
+            // keep_insert <= 4'b0111;
+            // byte_insert_cnt <= 3;
+            hdr_cnt = $urandom_range(1, DATA_BYTE_WD-1);
+		    keep_insert = 4'hf >> (DATA_BYTE_WD - hdr_cnt);
+		    byte_insert_cnt = hdr_cnt;
+        end
+    end
+end
+
+task data_valid; 
+    begin
+        data_sig <= 1;
+    end
+endtask
+task data_invalid; 
+    begin
+        data_sig <= 0;
+    end
+endtask
+task last; 
+    begin
+        last_sig <= 1;
+        #(PERIOD*1)
+        last_sig <= 0;
+    end
+endtask
+task head; 
+    begin
+        head_sig <= 1;
+        #(PERIOD*1)
+        head_sig <= 0;
+    end
+endtask
+
+task ready; 
+    begin
+        ready_out <= 1;
+    end
+endtask
+
+task notready; 
+    begin
+        ready_out <= 0;
+    end
+endtask
+
+reg [3:0] rand_val1;
+reg [3:0] rand_val2;
+reg [3:0] rand_val3;
+reg [31:0] seed=1;
+reg	[DATA_BYTE_WD-1:0] last_cnt;
+reg	[BYTE_CNT_WD-1:0] hdr_cnt;
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        
+    end else begin
+        rand_val1 = $random(seed);
+        rand_val2 = $random(seed);
+        rand_val3 = $random(seed);
+        if (rand_val1 < 10) begin
+            data_valid;
+            if (rand_val1 < 2)
+                last;
+        end else begin
+            data_invalid;
+        end
+
+        if (rand_val2 < 10) begin
+            head;
+        end
+        if (rand_val3 < 14) begin
+            ready;
+        end else begin
+            notready;
+        end
+    end
+end
 initial
 begin
-    valid_in <= 1;
-    last_in <= 0;
-
-    ready_out <= '1;
-    valid_insert <= '0;
-
-    data_insert <= 32'hAA55AA55;
-    keep_insert <= 4'b0111;
-    byte_insert_cnt <= 'd3;
-    #(PERIOD*5)
-    valid_insert <= 1;
-    #(PERIOD*1)
-    valid_insert <= 0;
-    #(PERIOD*2)
-    ready_out <= '0;
-    #(PERIOD*4)
-    ready_out <= '1;
-    #(PERIOD*5)
-    last_in <= 1;
-
-    #(PERIOD*2)
-    valid_insert <= 1;
-    data_insert <= 32'hAA55AA66;
-    keep_insert <= 4'b0001;
-    byte_insert_cnt <= 'd1;
-    valid_in <= 1;
-    #(PERIOD*1)
-    valid_insert <= 0;
-    #(PERIOD*5)
-    last_in <= 1;
+    head_sig <= 0;
+    data_sig <= 0;
+    last_sig <= 0;
+    ready_out <= 1;
 
 
-    #(PERIOD*10)
+    #(PERIOD*100)
     $finish;
 end
 
